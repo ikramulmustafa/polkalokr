26cabc6e129da7aa10aa176d583b26feb0ab3cb6
Ikram UL Mustafa <rao.ikram42@gmail.com>
2021-11-11 21:54:46 +0500

    package installation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 Vue.config.productionTip = false
 
 new Vue({
